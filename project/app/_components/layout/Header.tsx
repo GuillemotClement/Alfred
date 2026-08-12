efc42fa69a7168c89fdeec442bbe3ccef074eff7
GuillemotClement <guillemot.clement@protonmail.com>
2026-08-12 17:23:35 +0200

    feat(auth): update header and add logout

--- project/app/_components/layout/Header.tsx
+++ project/app/_components/layout/Header.tsx
@@ -1,6 +1,23 @@
-import Link from "next/link";
+"use client";
 
-export default function Header(){
+import { authClient } from "@/lib/auth-client";
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+export default function Header() {
+  const { data: session } = authClient.useSession();
+
+  const imgProfil =
+    session?.user.image || "https://randomuser.me/api/portraits/lego/3.jpg";
+
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    await authClient.signOut();
+    router.push("/");
+  };
+
   return (
     <div className="navbar bg-gray-600 shadow-sm text-white">
       <div className="flex-1">
@@ -8,72 +25,66 @@ export default function Header(){
           Alfred
         </Link>
       </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <Link href="/house">Foyer</Link>
-          </li>
-          <li>
-            <Link href="/login" className="btn btn-primary">Connexion</Link>
-          </li>
-          <li>
-            <Link href="/register" className="btn btn-neutral">Inscription</Link>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="bg-base-100 rounded-t-none p-2">
-                <li>
-                  <a>Link 1</a>
-                </li>
-                <li>
-                  <a>Link 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-        </ul>
-      </div>
-      <div className="flex gap-2">
-        <input
-          type="text"
-          placeholder="Search"
-          className="input w-24 md:w-auto"
-        />
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-              />
+
+      {session ? (
+        <div className="flex-none flex items-center gap-x-5">
+          <ul>
+            <li>
+              <Link href="/house">Foyer</Link>
+            </li>
+          </ul>
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <Image
+                  src={imgProfil}
+                  width={40}
+                  height={40}
+                  alt={session.user.name}
+                />
+              </div>
             </div>
+            <ul
+              tabIndex={-1}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              {/* <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li> */}
+              <li>
+                <button className="btn btn-warning" onClick={handleLogout}>
+                  Déconnexion
+                </button>
+              </li>
+            </ul>
           </div>
-          <ul
-            tabIndex={-1}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-          >
+        </div>
+      ) : (
+        <div className="flex-none">
+          <ul>
             <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
+              <Link href="/login" className="btn btn-primary">
+                Connexion
+              </Link>
             </li>
             <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
+              <Link href="/register" className="btn btn-neutral">
+                Inscription
+              </Link>
             </li>
           </ul>
         </div>
-      </div>
+      )}
     </div>
   );
 }
-
-
