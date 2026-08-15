483e6d70fc2c9579b58cc151d00c910d950f4cb6
GuillemotClement <guillemot.clement@protonmail.com>
2026-08-15 17:29:07 +0200

    fix

--- project/app/workplace/new/page.tsx
+++ project/app/workplace/new/page.tsx
@@ -1,7 +1,9 @@
+import { connection } from "next/server";
 import NewWorkplaceForm from "../_components/NewWorkplaceForm";
 import { getWorkplaceCategories } from "../_services/workplace";
 
 const NewWorkplacePage = async () => {
+  await connection();
   const categories = await getWorkplaceCategories();
 
   return (
